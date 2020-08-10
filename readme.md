d6f6592718135200625c510625f700710757f329
Tran Van Hoang <hoang.tv173129@sis.hust.edu.vn>
2020-08-11 00:08:40 +0700

    done real time detect video but not fast enoght

---
+++ readme.md
@@ -0,0 +1,8 @@
+# yolov4 face
+model **[yolov4-416-face](https://drive.google.com/drive/folders/14jkY84nEdJeJjGypyTrrvqCH2CpEL_z-?usp=sharing)** train from yolov4 for custom face detector using wider-face data set 
+
+you can use label_images.py for convert data from wider-face to darknet style for training (just change some directory depending on your project)
+ 
+i convert my trained darknet model to keras by using [tensorflow-yolov4-tflite](https://github.com/hunglc007/tensorflow-yolov4-tflite).
+note that you might want to change the obj.names file in config.py to your custom names file
+#
---
+++ source/detect_face.py
@@ -0,0 +1,106 @@
+import colorsys
+
+import numpy as np
+import tensorflow as tf
+from PIL import Image
+from tensorflow import keras
+import cv2
+import random
+import time
+
+
+def read_class_names(class_file_name):
+    names = {}
+    with open(class_file_name, 'r') as data:
+        for ID, name in enumerate(data):
+            names[ID] = name.strip('\n')
+    return names
+
+
+def draw_bbox(image, bboxes, classes="../classes/face.names", show_label=True):
+    classes = read_class_names(classes)
+    num_classes = len(classes)
+    image_h, image_w, _ = image.shape
+    hsv_tuples = [(1.0 * x / num_classes, 1., 1.) for x in range(num_classes)]
+    colors = list(map(lambda x: colorsys.hsv_to_rgb(*x), hsv_tuples))
+    colors = list(map(lambda x: (int(x[0] * 255), int(x[1] * 255), int(x[2] * 255)), colors))
+
+    random.seed(0)
+    random.shuffle(colors)
+    random.seed(None)
+
+    out_boxes, out_scores, out_classes, num_boxes = bboxes
+    for i in range(num_boxes[0]):
+        if int(out_classes[0][i]) < 0 or int(out_classes[0][i]) > num_classes: continue
+        coor = out_boxes[0][i]
+        coor[0] = int(coor[0] * image_h)
+        coor[2] = int(coor[2] * image_h)
+        coor[1] = int(coor[1] * image_w)
+        coor[3] = int(coor[3] * image_w)
+
+        fontScale = 0.5
+        score = out_scores[0][i]
+        class_ind = int(out_classes[0][i])
+        bbox_color = colors[class_ind]
+        bbox_thick = int(0.6 * (image_h + image_w) / 600)
+        c1, c2 = (coor[1], coor[0]), (coor[3], coor[2])
+        cv2.rectangle(image, c1, c2, bbox_color, bbox_thick)
+
+        if show_label:
+            bbox_mess = '%s: %.2f' % (classes[class_ind], score)
+            t_size = cv2.getTextSize(bbox_mess, 0, fontScale, thickness=bbox_thick // 2)[0]
+            c3 = (c1[0] + t_size[0], c1[1] - t_size[1] - 3)
+            cv2.rectangle(image, c1, (np.float32(c3[0]), np.float32(c3[1])), bbox_color, -1)  # filled
+
+            cv2.putText(image, bbox_mess, (c1[0], np.float32(c1[1] - 2)), cv2.FONT_HERSHEY_SIMPLEX,
+                        fontScale, (0, 0, 0), bbox_thick // 2, lineType=cv2.LINE_AA)
+    return image
+
+
+if __name__ == '__main__':
+    input_size = 416
+    image_path = '../image/bao_ngu.jpg'
+    model_path = '../models/keras_model/yolov4-416-face'
+
+    iou = 0.45
+    score = 0.25
+
+    saved_model_loaded = tf.keras.models.load_model(model_path)
+    infer = saved_model_loaded.signatures['serving_default']
+
+    original_image = cv2.imread(image_path)
+    original_image = cv2.cvtColor(original_image, cv2.COLOR_BGR2RGB)
+
+    image_data = cv2.resize(original_image, (input_size, input_size))
+    image_data = image_data / 255.
+
+    images_data = []
+    for i in range(1):
+        images_data.append(image_data)
+    images_data = np.asarray(images_data).astype(np.float32)
+    batch_data = tf.constant(images_data)
+    seconds = time.time()
+    print(batch_data.shape)
+    pred_bbox = infer(batch_data)
+    for key, value in pred_bbox.items():
+        boxes = value[:, :, 0:4]
+        pred_conf = value[:, :, 4:]
+
+    boxes, scores, classes, valid_detections = tf.image.combined_non_max_suppression(
+        boxes=tf.reshape(boxes, (tf.shape(boxes)[0], -1, 1, 4)),
+        scores=tf.reshape(
+            pred_conf, (tf.shape(pred_conf)[0], -1, tf.shape(pred_conf)[-1])),
+        max_output_size_per_class=50,
+        max_total_size=50,
+        iou_threshold=iou,
+        score_threshold=score
+    )
+    pred_bbox = [boxes.numpy(), scores.numpy(), classes.numpy(), valid_detections.numpy()]
+    image = draw_bbox(original_image, pred_bbox)
+    # image = utils.draw_bbox(image_data*255, pred_bbox)
+    image = Image.fromarray(image.astype(np.uint8))
+    image.show()
+    print(time.time() - seconds)
+    # image = cv2.cvtColor(np.array(image), cv2.COLOR_BGR2RGB)
+    # cv2.imwrite("result.png", image)
+
